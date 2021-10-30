251a47df50b4688f574fecad2c29d2858726a214
lvyufeng <lvyufeng@cqu.edu.cn>
2021-10-30 10:09:16 +0000

    add image classification dataset

--- d2l/mindspore.py
+++ d2l/mindspore.py
@@ -1,3 +1,4 @@
+import time
 import mindspore
 import numpy as np
 import mindspore.ops as ops
@@ -57,3 +58,31 @@ def synthetic_data(w, b, num_examples):
     y = np.matmul(X, w) + b
     y += np.random.normal(0, 0.01, y.shape)
     return X.astype(np.float32), y.reshape((-1, 1)).astype(np.float32)
+
+class Timer:
+    """记录多次运行时间。"""
+    def __init__(self):
+        """Defined in :numref:`subsec_linear_model`"""
+        self.times = []
+        self.start()
+
+    def start(self):
+        """启动计时器。"""
+        self.tik = time.time()
+
+    def stop(self):
+        """停止计时器并将时间记录在列表中。"""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """返回平均时间。"""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """返回时间总和。"""
+        return sum(self.times)
+
+    def cumsum(self):
+        """返回累计时间。"""
+        return np.array(self.times).cumsum().tolist()

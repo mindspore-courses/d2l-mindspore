8c560dc11c2dbc6c5633a048119d6c4f68992067
lvyufeng <lvyufeng@cqu.edu.cn>
2021-10-31 13:56:09 +0000

    add softmax-regression-scratch

--- d2l/mindspore.py
+++ d2l/mindspore.py
@@ -1,10 +1,50 @@
 import time
 import mindspore
 import numpy as np
+import mindspore.nn as nn
 import mindspore.ops as ops
 from mindspore import Tensor
 from matplotlib import pyplot as plt
 from IPython import display
+import mindspore.dataset.vision.c_transforms as cv
+import mindspore.dataset as ds
+
+def load_data_fashion_mnist(data_path, batch_size, resize=None, works=1):  
+    """将Fashion-MNIST数据集加载到内存中。"""
+    mnist_train = ds.MnistDataset(data_path, usage='train')
+    mnist_test = ds.MnistDataset(data_path, usage='test')
+    if resize:
+        trans = [cv.Resize(resize)]
+        mnist_train = mnist_train.map(trans, input_columns=["image"])
+        mnist_test = mnist_test.map(trans, input_columns=["image"])
+    mnist_train = mnist_train.batch(batch_size, num_parallel_workers=works)
+    mnist_test = mnist_test.batch(batch_size, num_parallel_workers=works)
+    return mnist_train, mnist_test
+
+def get_fashion_mnist_labels(labels):
+    """Return text labels for the Fashion-MNIST dataset.
+    Defined in :numref:`sec_utils`"""
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
+    """Plot a list of images.
+    Defined in :numref:`sec_utils`"""
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        try:
+            img = img.asnumpy()
+        except:
+            pass
+        ax.imshow(img)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
 
 def use_svg_display():
     """Use the svg format to display a plot in Jupyter.
@@ -86,3 +126,39 @@ class Timer:
     def cumsum(self):
         """返回累计时间。"""
         return np.array(self.times).cumsum().tolist()
+
+class SGD(nn.Cell):
+    def __init__(self, lr, batch_size, params):
+        super().__init__()
+        self.lr = lr
+        self.batch_size = batch_size
+        self.params = params
+        
+    def construct(self, grads):
+        for idx in range(len(self.params)):
+            ops.assign(self.params[idx], self.params[idx] - self.lr * grads[idx] / self.batch_size)
+        return True
+    
+class Train(nn.Cell):
+    def __init__(self, network, optimizer):
+        super().__init__()
+        self.network = network
+        self.optimizer = optimizer
+        self.grad = ops.GradOperation(get_by_list=True)
+        
+    def construct(self, x, y):
+        loss = self.network(x, y)
+        grads = self.grad(self.network, self.optimizer.params)(x, y)
+        loss = ops.depend(loss, self.optimizer(grads))
+        return loss
+
+class NetWithLoss(nn.Cell):
+    def __init__(self, network, loss):
+        super().__init__()
+        self.network = network
+        self.loss = loss
+        
+    def construct(self, x, y):
+        y_hat = self.network(x)
+        loss = self.loss(y_hat, y)
+        return loss

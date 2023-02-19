99e03a28e4f6ba3e74de06207ea3dd335bd65186
cqu_lxy <laixinyi@cqu.edu.cn>
2023-02-19 06:11:46 +0000

    update chapter14

--- d2l/mindspore.py
+++ d2l/mindspore.py
@@ -33,6 +33,8 @@ DATA_HUB['time_machine'] = (DATA_URL + 'timemachine.txt',
 DATA_HUB['fra-eng'] = (DATA_URL + 'fra-eng.zip',
                            '94646ad1522d915e7b0f9296181140edcf86a4f5')
 
+
+
 class Timer:
     """记录多次运行时间。"""
     def __init__(self):
@@ -332,6 +334,12 @@ def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
         if titles:
             ax.set_title(titles[i])
     return axes
+
+def get_dataloader_workers():
+    """Use 4 processes to read the data.
+    
+    Defined in :numref:`sec_fashion_mnist`"""
+    return 4
 
 def use_svg_display():
     """Use the svg format to display a plot in Jupyter.
@@ -1285,6 +1293,103 @@ def show_trace_2d(f, results):
 d2l.DATA_HUB['airfoil'] = (d2l.DATA_URL + 'airfoil_self_noise.dat',
                            '76e5be1548fd8222e5074cf0faae75edff8cf93f')
 
+d2l.DATA_HUB['ptb'] = (d2l.DATA_URL + 'ptb.zip',
+                       '319d85e578af0cdc590547f26231e4e31cdf1e42')
+
+def read_ptb():
+    """将PTB数据集加载到文本行的列表中"""
+    data_dir = d2l.download_extract('ptb')
+    # Readthetrainingset.
+    with open(os.path.join(data_dir, 'ptb.train.txt')) as f:
+        raw_text = f.read()
+    return [line.split() for line in raw_text.split('\n')]
+
+def subsample(sentences, vocab):
+    """下采样高频词"""
+    # 排除未知词元'<unk>'
+    sentences = [[token for token in line if vocab[token] != vocab.unk]
+                 for line in sentences]
+    counter = d2l.count_corpus(sentences)
+    num_tokens = sum(counter.values())
+
+    # 如果在下采样期间保留词元，则返回True
+    def keep(token):
+        return(random.uniform(0, 1) <
+               math.sqrt(1e-4 / counter[token] * num_tokens))
+
+    return ([[token for token in line if keep(token)] for line in sentences],
+            counter)
+
+def get_centers_and_contexts(corpus, max_window_size):
+    """返回跳元模型中的中心词和上下文词"""
+    centers, contexts = [], []
+    for line in corpus:
+        # 要形成“中心词-上下文词”对，每个句子至少需要有2个词
+        if len(line) < 2:
+            continue
+        centers += line
+        for i in range(len(line)):  # 上下文窗口中间i
+            window_size = random.randint(1, max_window_size)
+            indices = list(range(max(0, i - window_size),
+                                 min(len(line), i + 1 + window_size)))
+            # 从上下文词中排除中心词
+            indices.remove(i)
+            contexts.append([line[idx] for idx in indices])
+    return centers, contexts
+
+class RandomGenerator:
+    """根据n个采样权重在{1,...,n}中随机抽取"""
+    def __init__(self, sampling_weights):
+        # Exclude
+        self.population = list(range(1, len(sampling_weights) + 1))
+        self.sampling_weights = sampling_weights
+        self.candidates = []
+        self.i = 0
+
+    def draw(self):
+        if self.i == len(self.candidates):
+            # 缓存k个随机采样结果
+            self.candidates = random.choices(
+                self.population, self.sampling_weights, k=10000)
+            self.i = 0
+        self.i += 1
+        return self.candidates[self.i - 1]
+
+def get_negatives(all_contexts, vocab, counter, K):
+    """返回负采样中的噪声词"""
+    # 索引为1、2、...（索引0是词表中排除的未知标记）
+    sampling_weights = [counter[vocab.to_tokens(i)]**0.75
+                        for i in range(1, len(vocab))]
+    all_negatives, generator = [], RandomGenerator(sampling_weights)
+    for contexts in all_contexts:
+        negatives = []
+        while len(negatives) < len(contexts) * K:
+            neg = generator.draw()
+            # 噪声词不能是上下文词
+            if neg not in contexts:
+                negatives.append(neg)
+        all_negatives.append(negatives)
+    return all_negatives
+
+def batchify(*data):
+    """返回带有负采样的跳元模型的小批量样本"""
+    _data = data
+    if isinstance(data[0], list):
+        data = zip(data[0], data[1], data[2])
+        _data = zip(_data[0], _data[1], _data[2])
+    max_len = max(len(c) + len(n) for _, c, n in data)
+    centers, contexts_negatives, masks, labels = [], [], [], []
+    for center, context, negative in _data:
+        if not isinstance(center, (int, list)):
+            center, context, negative = center.tolist(), context.tolist(), negative.tolist()
+        cur_len = len(context) + len(negative)
+        centers += [center]
+        contexts_negatives += \
+            [context + negative + [0] * (max_len - cur_len)]
+        masks += [[1] * cur_len + [0] * (max_len - cur_len)]
+        labels += [[1] * len(context) + [0] * (max_len - len(context))]
+    return (np.array(centers).reshape((-1, 1)), np.array(contexts_negatives), np.array(masks), np.array(labels))
+
 def get_data_ch11(batch_size=10, n=1500):
     """Defined in :numref:`sec_minibatches`"""
     data = np.genfromtxt(d2l.download('airfoil'),
@@ -1293,6 +1398,40 @@ def get_data_ch11(batch_size=10, n=1500):
     data_iter = d2l.load_array((data[:n, :-1], data[:n, -1]),
                                batch_size, is_train=True)
     return data_iter, data.shape[1]-1
+
+def load_data_ptb(batch_size, max_window_size, num_noise_words):
+    """下载PTB数据集，然后将其加载到内存中"""
+    num_workers = d2l.get_dataloader_workers()
+    sentences = read_ptb()
+    vocab = d2l.Vocab(sentences, min_freq=10)
+    subsampled, counter = subsample(sentences, vocab)
+    corpus = [vocab[line] for line in subsampled]
+    all_centers, all_contexts = get_centers_and_contexts(
+        corpus, max_window_size)
+    all_negatives = get_negatives(
+        all_contexts, vocab, counter, num_noise_words)
+
+    class PTBDataset:
+        def __init__(self, centers, contexts, negatives):
+            assert len(centers) == len(contexts) == len(negatives)
+            self.centers = centers
+            self.contexts = contexts
+            self.negatives = negatives
+
+        def __getitem__(self, index):
+            return (self.centers[index], self.contexts[index],
+                    self.negatives[index])
+
+        def __len__(self):
+            return len(self.centers)
+    
+    data = PTBDataset(all_centers, all_contexts, all_negatives)
+    dataset = mindspore.dataset.GeneratorDataset(data, column_names=["center", "context", "negative"], shuffle=True,
+                                                 num_parallel_workers=num_workers)
+    dataset = dataset.batch(batch_size=512, per_batch_map=batchify,
+                            output_columns=['centers', 'contexts_negatives', 'masks', 'labels'])
+    
+    return dataset, vocab
 
 def train_ch11(trainer_fn, states, hyperparams, data_iter,
                feature_dim, num_epochs=2):
